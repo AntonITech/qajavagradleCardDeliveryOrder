756d90fc6726812a1ff652cd599e1c064dc8113e
AntonI <avmiass@bk.ru>
2023-03-17 15:40:52 +0300

    fixed

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldRegisterByAccountDeliveryCard() {
+        open("http://localhost:9999/");
+        Configuration.holdBrowserOpen = true;
+    }
+}
+
+
+
+
