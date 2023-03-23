d523d663f48fea8786d722eed3bab0df586cd3a1
AntonI <avmiass@bk.ru>
2023-03-23 11:40:02 +0300

    last edit test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,23 +1,37 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void shouldRegisterByAccountDeliveryCard() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+        String planningDate = generateDate(4);
         $x("//input[@type='text']").setValue("Санкт-Петербург");
-        $x("//input[@type='tel']").setValue("28.03.2023");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Иванов-Петров Иван");
         $x("//input[@name='phone']").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(),'Забронировать')]").click();
         Configuration.timeout = 15000;
         $x("//*[contains(@data-test-id, 'notification')]").shouldBe(Condition.visible);
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
 
